6a206a2a61c639414244389031405953f2bfe4df
kinsum <79512621+kinsum666@users.noreply.github.com>
2023-01-01 15:29:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -50,7 +50,7 @@ git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dns
 
 #添加额外软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-# git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
